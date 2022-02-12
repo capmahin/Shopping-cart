fc758b088ec2bae5afc4509fb11dc9a1e48f0fec
capmahin <mahinulhuque1996@gmail.com>
2022-02-13 05:50:53 +0600

    update new fuction adding new id case-minus case-total create two condition

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 //staring cart
-document.getElementById("case-plus").addEventListener("click", function () {
+function updateCaseNumber(isIncreasing) {
   const caseInput = document.getElementById("case-number");
-  const caseNumber = caseInput.value;
-  caseInput.value = parseInt(caseNumber) + 1;
+  let caseNumber = caseInput.value;
+  if (isIncreasing == true) {
+    caseNumber = parseInt(caseNumber) + 1;
+  } else if (caseNumber > 0) {
+    caseNumber = parseInt(caseNumber) - 1;
+  }
+  caseInput.value = caseNumber;
+  const caseTotal = document.getElementById("case-total");
+  caseTotal.innerText = caseNumber * 59;
+}
+document.getElementById("case-plus").addEventListener("click", function () {
+  updateCaseNumber(true);
+});
+document.getElementById("case-minus").addEventListener("click", function () {
+  updateCaseNumber(false);
 });
