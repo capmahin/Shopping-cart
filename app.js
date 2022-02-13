0aa9ba0d8cff599ab168d2d6facb416a17d2660c
capmahin <mahinulhuque1996@gmail.com>
2022-02-13 07:39:51 +0600

    adding getinputvalue fuction and calculate fuction get new three id  sub-total tax-amount total-price declare phone and case in getinputvalue and its calculate in calculate fuction

--- app.js
+++ app.js
@@ -11,6 +11,25 @@ function updateProductNumber(product, price, isIncreasing) {
   //   total product value
   const caseTotal = document.getElementById(product + "-total");
   caseTotal.innerText = productNumber * price;
+  //   calculate total
+  calculateTotal();
+}
+//  calculate total tex and total price
+function getInputValue(product) {
+  const productInput = document.getElementById(product + "-number");
+  const productNumber = parseInt(productInput.value);
+  return productNumber;
+}
+function calculateTotal() {
+  const phoneTotal = getInputValue("phone") * 1219;
+  const caseTotal = getInputValue("case") * 59;
+  const subTotal = phoneTotal + caseTotal;
+  const tax = subTotal / 10;
+  const totalPrice = subTotal + tax;
+  //   update on the Html
+  document.getElementById("sub-total").innerText = subTotal;
+  document.getElementById("tax-amount").innerText = tax;
+  document.getElementById("total-price").innerText = totalPrice;
 }
 // phone increasing and decreasing
 document.getElementById("phone-plus").addEventListener("click", function () {
